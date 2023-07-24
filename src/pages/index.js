0cf09cbd4054bab95f615ecf3ba27cf302b34b1e
SergeiGorbunov <fantasycraft2000@gmail.com>
2023-07-24 14:23:56 +0300

    fix

---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import EditArticle from './editArtcle'
+import ErrorPage from './errorPage'
+import NewArticle from './newArticle'
+import Profile from './profile'
+import SignIn from './singIn'
+import SignUp from './singUp'
+
+export { EditArticle, ErrorPage, NewArticle, Profile, SignIn, SignUp }
